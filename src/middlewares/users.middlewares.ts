b04678a7c6b9fd1a8ba075eb7b0a53e4ed843454
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-06-22 15:10:12 +0700

    +Add: validation with express validator

---
+++ src/middlewares/users.middlewares.ts
@@ -0,0 +1,92 @@
+import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
+
+export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      error: 'Missing email or password'
+    })
+  }
+  next()
+}
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 0,
+          max: 100
+        }
+      },
+      trim: true
+    },
+    email: {
+      isEmail: true,
+      notEmpty: true,
+      trim: true
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage:
+          'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbols'
+      }
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage:
+          'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbols'
+      },
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 
 const usersRouter = Router()
 
-usersRouter.post('/login', loginController)
-usersRouter.get('/register', registerController)
+/**
+ * Description: Register a user
+ * Path: /register
+ * Method: POST
+ * Body: { email: string, password: string }
+ */
+usersRouter.post('/login', loginValidator, loginController)
+
+/**
+ * Description: Register a user
+ * Path: /register
+ * Method: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
+ */
+usersRouter.get('/register', registerValidator, registerController)
 
 export default usersRouter
