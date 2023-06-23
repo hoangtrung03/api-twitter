7ae8adb8e4d03773f71206521b6bcddd1f1d074a
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-06-23 17:03:26 +0700

    +Update: handle error

---
+++ src/constants/httpStatus.ts
@@ -0,0 +1,8 @@
+export const httpStatus = {
+  OK: 200,
+  UNPROCESSABLE_ENTITY: 422,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500
+}
+
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,7 @@
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
 import express from 'express'
 import { validationResult, ValidationChain } from 'express-validator'
+import { ErrorWithStatus } from '~/models/Errors'
 // can be reused by many routes
 
 // sequential processing, stops running validations chain if the previous one fails.
@@ -8,10 +9,17 @@ export const validate = (validation: RunnableValidationChains<ValidationChain>) 
   return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     await validation.run(req)
     const errors = validationResult(req)
+    const errorsObject = errors.mapped()
+    for (const key in errorsObject) {
+      const { msg } = errorsObject[key]
+      if (msg instanceof ErrorWithStatus && msg.status === 422) {
+        errorsObject[key] = errorsObject[key].msg
+      }
+    }
     if (errors.isEmpty()) {
       return next()
     }
 
-    res.status(400).json({ errors: errors.mapped() })
+    res.status(400).json({ errors: errorsObject })
   }
 }
