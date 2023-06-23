bb9942877b58c55321352fad4a4c0d2eb1980b57
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-06-23 11:28:40 +0700

    +Update: expired time

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,31 +1,21 @@
 import { Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterRequestBody } from '~/models/requests/User.requests'
-import usersService from '~/services/users.services'
+import userService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
-  if (email === 'trung@gmail.com' && password === '123456') {
-    return res.status(200).json({
-      message: 'Login success'
-    })
-  }
-  return res.status(400).json({
-    error: 'Login failed'
-  })
 }
 
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterRequestBody>, res: Response) => {
   try {
-    const result = await usersService.register(req.body)
+    const result = await userService.register(req.body)
     return res.json({
-      message: 'Register success',
+      message: 'Registration successful',
       result
     })
   } catch (error) {
-    return res.status(400).json({
-      message: 'Register failed',
-      error
-    })
+    console.log(error)
+    return res.status(400).json({ message: 'Registration failed', error })
   }
 }
