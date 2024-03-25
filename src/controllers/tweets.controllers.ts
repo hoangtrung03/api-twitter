3b72f0ff58fa56c1c19dd8af262a88bfb51e1cb1
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-03-25 08:53:54 +0700

    +Add: create tweet api

--- src/controllers/tweets.controllers.ts
+++ src/controllers/tweets.controllers.ts
@@ -2,19 +2,13 @@ import { Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { TWEETS_MESSAGES } from '~/constants/messages'
 import { TweetRequestBody } from '~/models/requests/Tweet.request'
+import { TokenPayload } from '~/models/requests/User.requests'
 import { default as tweetsService } from '~/services/tweets.services'
 
 export const createTweetController = async (req: Request<ParamsDictionary, any, TweetRequestBody>, res: Response) => {
+  const { user_id } = req.decoded_authorization as TokenPayload
   const { audience, content, hashtags, medias, mentions, parent_id, type } = req.body
-  const result = await tweetsService.createTweet({
-    audience,
-    content,
-    hashtags,
-    medias,
-    mentions,
-    parent_id,
-    type
-  })
+  const result = await tweetsService.createTweet(user_id, req.body)
 
   return res.json({
     message: TWEETS_MESSAGES.CREATE_TWEET_SUCCESS,
--- src/services/tweets.services.ts
+++ src/services/tweets.services.ts
@@ -1,19 +1,28 @@
 import { config } from 'dotenv'
+import { ObjectId } from 'mongodb'
 import { TweetRequestBody } from '~/models/requests/Tweet.request'
+import Tweet from '~/models/schemas/Tweet.schema'
+import databaseService from './database.services'
 
 config()
 
 class TweetsService {
-  async createTweet({ audience, content, hashtags, medias, mentions, parent_id, type }: TweetRequestBody) {
-    return {
-      audience,
-      content,
-      hashtags,
-      medias,
-      mentions,
-      parent_id,
-      type
-    }
+  async createTweet(user_id: string, body: TweetRequestBody) {
+    const result = await databaseService.tweets.insertOne(
+      new Tweet({
+        audience: body.audience,
+        content: body.content,
+        hashtags: [],
+        mentions: body.mentions,
+        medias: body.medias,
+        parent_id: body.parent_id,
+        type: body.type,
+        user_id: new ObjectId(user_id)
+      })
+    )
+    const tweet = await databaseService.tweets.findOne({ _id: result.insertedId })
+
+    return tweet
   }
 }
 
